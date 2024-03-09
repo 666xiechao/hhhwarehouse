fab4e80fa138fba6237220e3c58626c9ec985e3a
666xiechao <2915622120@qq.com>
2024-03-09 20:12:52 +0800

    hhhwarehouse

---
+++ src/test/java/edu/njuit/xc/springbootlearn/SpringBootLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.njuit.xc.springbootlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
